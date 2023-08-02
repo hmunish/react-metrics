eb56ac01678325ee502f70df562662547db35531
Munish <hmunish5@gmail.com>
2023-08-02 23:26:37 +0200

    Add currency filter feature

--- src/redux/currency/currencySlice.js
+++ src/redux/currency/currencySlice.js
@@ -1,48 +1,58 @@
-import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 
-const API_URL = 'https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies';
+const API_URL =
+  "https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies";
 
 const initialState = {
   currenciesData: [],
-  curCountryCode: '',
+  currenciesDataCopy: [],
+  curCountryCode: "",
   isLoading: false,
   isError: false,
 };
 
 export const fetchAllCurrencies = createAsyncThunk(
-  'currency/fetchCurrencies',
+  "currency/fetchCurrencies",
   async (thunkAPI) => {
     try {
       const res = await fetch(`${API_URL}.json`);
-      if (!res.ok) throw new Error('Error fetching currencies data');
+      if (!res.ok) throw new Error("Error fetching currencies data");
       const data = await res.json();
       return data;
     } catch (err) {
-      return thunkAPI.rejectWithValue('Something went wrong');
+      return thunkAPI.rejectWithValue("Something went wrong");
     }
-  },
+  }
 );
 
 export const fetchCurrencyData = createAsyncThunk(
-  'currency/fetchCurrency',
+  "currency/fetchCurrency",
   async (curCode, thunkAPI) => {
     try {
       const res = await fetch(`${API_URL}/${curCode}.json`);
-      if (!res.ok) throw new Error('Error fetching currency data');
+      if (!res.ok) throw new Error("Error fetching currency data");
       const data = await res.json();
       return data;
     } catch (err) {
-      return thunkAPI.rejectWithValue('Something went wrong');
+      return thunkAPI.rejectWithValue("Something went wrong");
     }
-  },
+  }
 );
 
 export const currencySlice = createSlice({
-  name: 'currencySlice',
+  name: "currencySlice",
   initialState,
   reducers: {
+    handleSearch: (state, query) => {
+      const tmp = Object.entries(state.currenciesDataCopy).filter((el) => {
+        const q = query.payload;
+        console.log(q);
+        return el[1].includes(q);
+      });
+      state.currenciesData = Object.fromEntries(tmp);
+    },
     clearCountryCode: (state) => {
-      state.curCountryCode = '';
+      state.curCountryCode = "";
     },
     setCountryCode: (state, param) => {
       state.curCountryCode = param.payload;
@@ -53,6 +63,7 @@ export const currencySlice = createSlice({
       state.isLoading = false;
       state.isError = false;
       state.currenciesData = action.payload;
+      state.currenciesDataCopy = action.payload;
     });
     builder.addCase(fetchAllCurrencies.pending, (state) => {
       state.isLoading = true;
@@ -76,5 +87,6 @@ export const currencySlice = createSlice({
   },
 });
 
-export const { clearCountryCode, setCountryCode } = currencySlice.actions;
+export const { clearCountryCode, setCountryCode, handleSearch } =
+  currencySlice.actions;
 export default currencySlice.reducer;
