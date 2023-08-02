a4a1ce1ff0391ad55b47cc608109c769b389666e
Munish <hmunish5@gmail.com>
2023-08-02 09:55:38 +0200

    Add navbar markup

---
+++ src/components/navbar.js
@@ -0,0 +1,17 @@
+import { NavLink } from "react-router-dom";
+function Navbar() {
+  return (
+    <nav>
+      <NavLink to="/">&#91;</NavLink>
+      <p>All currencies</p>
+      <ul>
+        <li>
+          <img></img>
+        </li>
+        <li>
+          <img></img>
+        </li>
+      </ul>
+    </nav>
+  );
+}
